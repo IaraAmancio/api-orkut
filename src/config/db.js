28abe27048f6da59d1de1c8c1d6cc9ccd1e517bf
unknown <iaraamancio1986@gmail.com>
2026-04-08 10:15:02 -0300

    último deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
 const {Pool} = require("pg");
+const { ssl } = require("pg/lib/defaults");
 
 module.exports = new Pool({
     connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        }
 });
 
